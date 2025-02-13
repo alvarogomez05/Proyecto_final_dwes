34f57e90c54f3fac674f35beff407da5d27b3347
ricardo <rihozmar@gmail.com>
2025-02-13 19:21:42 +0100

    Controladores terminados

---
+++ backend/services/empleadosService.php
@@ -0,0 +1,35 @@
+<?php
+require_once '../database/models/empleadosModel.php';
+
+class EmpleadosService
+{
+
+    private $empleadosModel;
+
+    public function __construct()
+    {
+        $this->empleadosModel = new EmpleadosModel();
+    }
+
+
+    public function guardarEmpleado($id, $nombre, $apellido1, $apellido2, $cargo)
+    {
+        return $this->empleadosModel->save($id, $nombre, $apellido1, $apellido2, $cargo);
+    }
+
+    public function obtenerEmpleadoPorId($id)
+    {
+        return $this->empleadosModel->getEmpleadoById($id);
+    }
+
+
+    public function obtenerTodosLosEmpleados()
+    {
+        return $this->empleadosModel->getAll();
+    }
+
+    public function borrarEmpleado($id)
+    {
+        return $this->empleadosModel->borrar($id);
+    }
+}
