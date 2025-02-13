f3b9991f791e4f9f3e2f5c3ea8a5bed2fa69492d
Laura <laura.liquetegomez@riberadeltajo.es>
2025-02-13 19:26:11 +0100

    restauración de rama

--- backend/database/models/Perro.php
+++ backend/database/models/Perro.php
@@ -3,11 +3,11 @@
 class Perro
 {
 
-    public $id;
-    public $nombre;
-    public $raza;
-    public $edad;
-    public $dni_cliente;
+    protected $id;
+    protected $nombre;
+    protected $raza;
+    protected $edad;
+    protected $dni_cliente;
 
     // Constructor
     public function __construct($id, $nombre, $raza, $edad, $dni_cliente)
