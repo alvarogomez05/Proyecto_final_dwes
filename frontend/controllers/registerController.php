86756e20806335bf315ec75549c6bcf7f42c55c3
Alpego05 <alba2005pg@gmail.com>
2025-02-19 19:55:03 +0100

    commit

--- frontend/controllers/registerController.php
+++ frontend/controllers/registerController.php
@@ -1,6 +1,6 @@
 <?php
 // URL de la API donde se registrarán los empleados
-$url = 'http://localhost/Proyecto%20APIS/backend/?ruta=empleados';
+$url = 'http://localhost/perros/backend/?ruta=empleados';
 
 // Recibir datos del formulario
 $dni = $_GET['dni'] ?? '';
