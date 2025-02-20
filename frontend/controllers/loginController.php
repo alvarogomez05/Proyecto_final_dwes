50f4e88f65ba553f780e8dc27daeb117ed15638b
ricardo <rihozmar@gmail.com>
2025-02-20 20:15:04 +0100

    Comprobacion en el login para saber si es admin o empleado

--- frontend/controllers/loginController.php
+++ frontend/controllers/loginController.php
@@ -1,11 +1,19 @@
 <?php
+// session_start();
 if (isset($_POST['submit'])) {
-    $url = 'http://localhost/dwes/proyecto%20final/backend/?ruta=empleados';
+    $url = 'http://localhost/Proyecto%20APIS/backend/?ruta=empleados';
     $response = file_get_contents($url);
     $data = json_decode($response, true);
     $users = [];
+    
     foreach ($data as $user) {
-        $users[$user['email']] = $user['password']; // Guardamos en formato [email => password]
+        // $users[$user['email']] = $user['password'] ;
+        // Guardamos en formato [email => password]
+
+        $users[$user['email']] = [
+            'password' => $user['password'],
+            'rol' => $user['rol']  // Guardamos también el rol
+        ];
     }
 
     //Guardamos en la sesion el usuario que se ha metido
@@ -21,14 +29,19 @@ if (isset($_POST['submit'])) {
     echo "COntraseña de ese usuario es: " . $password;
     // Validar si el usuario existe
     if (isset($users[$email])) {
-        $storedPassword = $users[$email]; // Obtener la contraseña guardada
+        // $storedPassword = $users[$email]; // Obtener la contraseña guardada
+         $storedPassword = $users[$email]['password'];
+        $rol = $users[$email]['rol'];
         // Comparar contraseña (Si está en texto plano, usa ===. Si está hasheada, usa password_verify)
         if (password_verify($password, $storedPassword)) {
             $_SESSION['user'] = $email; // Guardar sesión del usuario
             echo "Inicio de sesión exitoso. ¡Bienvenido, $email!";
             // Redirigir a un dashboard u otra página
-            header("Location: ./pages/main.php");
-
+            if ($rol === 'ADMIN') {
+                header("Location: ./pages/main.php");
+            } else {
+                header("Location: ./pages/mainEmpleados.php");
+            }
         } else {
             echo "Error: Contraseña incorrecta.";
         }
